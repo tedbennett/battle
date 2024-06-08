521ca45d1f6ca5739d9845784006e24ce8e6553d
Ted Bennett <44608108+tedbennett@users.noreply.github.com>
2024-06-08 17:52:32 +0100

    add conway game of life streaming

--- board/board.go
+++ board/board.go
@@ -1,5 +1,7 @@
 package board
 
+import "math/rand/v2"
+
 // Board represents which squares are taken by which side
 type Board struct {
 	Squares [][]int
@@ -16,16 +18,6 @@ const (
 	Team2Color = "#eeafaf"
 )
 
-func teamToColor(team int) string {
-	switch team {
-	case Team1:
-		return Team1Color
-	case Team2:
-		return Team2Color
-	}
-	return "000000"
-}
-
 func NewBoard(size int, element int) Board {
 	squares := make([][]int, size)
 	for i := range size {
@@ -38,6 +30,10 @@ func NewBoard(size int, element int) Board {
 	return Board{Squares: squares}
 }
 
+// ========================================================
+// Server side rendering of board
+// ========================================================
+
 func (b *Board) Colors() [][]string {
 	colors := make([][]string, len(b.Squares))
 	for rowIdx, row := range b.Squares {
@@ -48,4 +44,61 @@ func (b *Board) Colors() [][]string {
 		colors[rowIdx] = colorRow
 	}
 	return colors
+}
+
+func teamToColor(team int) string {
+	switch team {
+	case Team1:
+		return Team1Color
+	case Team2:
+		return Team2Color
+	}
+	return "000000"
+}
+
+// ========================================================
+// Conways game of life
+// ========================================================
+
+func NewConwayBoard(size int) Board {
+	b := NewBoard(size, Team1)
+	for range size * 100 {
+		row, col := rand.IntN(size), rand.IntN(size)
+		b.Squares[row][col] = Team2
+	}
+	return b
+}
+
+func (b *Board) Tick() {
+	for rowIdx, row := range b.Squares {
+		for colIdx, col := range row {
+			neighbours := b.getNeighbours(rowIdx, colIdx)
+			if col == 1 {
+				if neighbours > 3 || neighbours < 2 {
+					b.Squares[rowIdx][colIdx] = 0
+				}
+			} else if neighbours == 3 {
+				b.Squares[rowIdx][colIdx] = 1
+			}
+		}
+	}
+}
+
+func (b *Board) getNeighbours(row int, col int) int {
+	count := 0
+	if row != 0 {
+		count += b.Squares[row-1][col]
+	}
+	if row != len(b.Squares[0])-1 {
+		count += b.Squares[row+1][col]
+	}
+
+	if col != 0 {
+		count += b.Squares[row][col-1]
+	}
+	// Assuming square board
+	if col != len(b.Squares[0])-1 {
+		count += b.Squares[row][col+1]
+	}
+	return count
 }
--- main.go
+++ main.go
@@ -30,27 +30,20 @@ func main() {
 	e.Static("static/js", "static/js")
 	templates.NewTemplateRenderer(e, "static/*.html")
 
-	b := board.NewBoard(5, 0)
+	b := board.NewConwayBoard(200)
 	colors := map[int]string{0: board.Team1Color, 1: board.Team2Color}
 	init := messages.NewInitMessage(colors, b.Squares)
 	initMsg, _ := json.Marshal(init)
 	channel := make(chan []byte)
 	go func(channel chan<- []byte) {
 		defer close(channel)
-		count := 0
 		for {
-			time.Sleep(time.Millisecond * 2000)
+			time.Sleep(time.Millisecond * 500)
+			b.Tick()
 			e.Logger.Info("Sending board message")
-			b := board.NewBoard(5, 0)
-			b.Squares[count/5][count%5] = 1
 			msg := messages.NewBoardMessage(b.Squares)
 			json, _ := json.Marshal(msg)
 			channel <- json
-			if count < 24 {
-				count += 1
-			} else {
-				count = 0
-			}
 		}
 	}(channel)
 	e.GET("/", routes.Home())
